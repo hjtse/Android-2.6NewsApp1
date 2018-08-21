595525b9a4b66aa21ca5371ce1f7038645d20844
Han John Tse <hanjohntse@gmail.com>
2018-08-21 15:36:44 -0700

    revert back, just a blue circle

--- app/src/main/java/com/example/android/news/NewsAdapter.java
+++ app/src/main/java/com/example/android/news/NewsAdapter.java
@@ -54,18 +54,6 @@ public class NewsAdapter extends ArrayAdapter<News> {
         // Display the section name of the current news in that TextView
         sectionNameView.setText(currentNews.getSectionName());
 
-        // Set the proper background color on the magnitude circle.
-        // Fetch the background from the TextView, which is a GradientDrawable.
-        GradientDrawable magnitudeCircle = (GradientDrawable) sectionNameView.getBackground();
-
-        // Get the appropriate background color based on the current earthquake magnitude
-        int magnitudeColor = getMagnitudeColor(currentNews.getSectionName());
-
-        // Set the color on the magnitude circle
-        magnitudeCircle.setColor(magnitudeColor);
-
-
-
 
         // Find the TextView with view ID date
         TextView dateView = null;
@@ -97,49 +85,6 @@ public class NewsAdapter extends ArrayAdapter<News> {
         return listItemView;
     }
 
-    /**
-     * Return the color for the magnitude circle based on the intensity of the earthquake.
-     *
-     * @param magnitude of the earthquake
-     */
-    private int getMagnitudeColor(double magnitude) {
-        int magnitudeColorResourceId;
-        int magnitudeFloor = (int) Math.floor(magnitude);
-        switch (magnitudeFloor) {
-            case 0:
-            case 1:
-                magnitudeColorResourceId = R.color.magnitude1;
-                break;
-            case 2:
-                magnitudeColorResourceId = R.color.magnitude2;
-                break;
-            case 3:
-                magnitudeColorResourceId = R.color.magnitude3;
-                break;
-            case 4:
-                magnitudeColorResourceId = R.color.magnitude4;
-                break;
-            case 5:
-                magnitudeColorResourceId = R.color.magnitude5;
-                break;
-            case 6:
-                magnitudeColorResourceId = R.color.magnitude6;
-                break;
-            case 7:
-                magnitudeColorResourceId = R.color.magnitude7;
-                break;
-            case 8:
-                magnitudeColorResourceId = R.color.magnitude8;
-                break;
-            case 9:
-                magnitudeColorResourceId = R.color.magnitude9;
-                break;
-            default:
-                magnitudeColorResourceId = R.color.magnitude10plus;
-                break;
-        }
-        return ContextCompat.getColor(getContext(), magnitudeColorResourceId);
-    }
 
     /**
      * Return the formatted date string (i.e. "Mar 3, 1984") from a Date object.
